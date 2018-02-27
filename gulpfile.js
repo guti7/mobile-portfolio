87d98d56ad1ef93769a5b50de2d21e03d8bcf7aa
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-26 17:06:54 -0800

    Serve files to browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
-var gulp = require('gulp');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload;
+
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+    browser: "google chrome"
+  })
+  gulp.watch(['index.html', 'css/**/*.css', 'js/**/*.js'], {cwd: './'}, reload);
+});
 
 gulp.task('default', defaultTask);
 
