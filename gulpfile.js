ca738e8032362b95b094e0be963c1d0f687141fd
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-26 21:59:12 -0800

    Optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 var gulp        = require('gulp'),
     browserSync = require('browser-sync'),
+    imagemin    = require('gulp-imagemin'),
+    del         = require('del'),
     reload      = browserSync.reload;
 
+gulp.task('clean', function(){
+  return del(['build/dist/images/']);
+});
+
+gulp.task('imagemin', function() {
+  return gulp.src(['img/**/*', 'views/images/**/*']) // 'views/images/**/*.{png, jpg}'
+        .pipe(imagemin([
+          imagemin.optipng({ optimizationLevel: 5 }),
+          imagemin.jpegtran({ progressive: true })
+        ]))
+        .pipe(gulp.dest('build/dist/images'))
+});
 
 // watch files for changes and reload
 gulp.task('serve', function() {
