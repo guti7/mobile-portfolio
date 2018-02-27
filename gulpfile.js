9dd29bd18c692acacc6b080de6a28013918ac738
guti7 <7505855+guti7@users.noreply.github.com>
2018-02-27 06:28:14 -0800

    Optimize rendering path with minified source files

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,44 @@ var gulp        = require('gulp'),
     del         = require('del'),
     cssnano     = require('gulp-cssnano'),
     uglify      = require('gulp-uglify'),
+    htmlmin     = require('gulp-htmlmin')
     reload      = browserSync.reload;
 
+var paths = {
+  styles: {
+    src: ['src/css/**/*.css'],
+    dest: 'build/dist/css/'
+  },
+  scripts: {
+    src: ['src/js/**/*.js'],
+    dest: 'build/dist/js'
+  },
+  html: {
+    src: ['src/*.html'],
+    dest: './'
+  },
+  images: {
+    src: ['src/img/**/*', 'src/views/images/**/*'],
+    dest: 'build/dist/images'
+  }
+}
+
+gulp.task('minify', function() {
+  return gulp.src(paths.html.src)
+         .pipe(htmlmin({collapseWhitespace : true}))
+         .pipe(gulp.dest(paths.html.dest))
+});
 
 gulp.task('scripts', function() {
-  return gulp.src(['js/*'])
+  return gulp.src(paths.scripts.src)
          .pipe(uglify())
-         .pipe(gulp.dest('build/dist/scripts'));
+         .pipe(gulp.dest(paths.scripts.dest));
 });
 
 gulp.task('styles', function() {
-  return gulp.src(['css/**/*.css'])
+  return gulp.src(paths.styles.src)
          .pipe(cssnano())
-         .pipe(gulp.dest('build/dist/styles/'));
+         .pipe(gulp.dest(paths.styles.dest));
 });
 
 gulp.task('clean', function() {
@@ -29,12 +54,12 @@ gulp.task('clear', function() {
 });
 
 gulp.task('imagemin', function() {
-  return gulp.src(['img/**/*', 'views/images/**/*']) // 'views/images/**/*.{png, jpg}'
+  return gulp.src(paths.images.src)
         .pipe(cache(imagemin([
           imagemin.optipng({ optimizationLevel: 5 }),
           imagemin.jpegtran({ progressive: true })
         ])))
-        .pipe(gulp.dest('build/dist/images'))
+        .pipe(gulp.dest(paths.images.dest));
 });
 
 // watch files for changes and reload
@@ -51,5 +76,5 @@ gulp.task('serve', function() {
 gulp.task('default', defaultTask());
 
 function defaultTask() {
-  return gulp.series('clean', gulp.parallel('scripts', 'styles', 'imagemin', 'serve'));
+  return gulp.series('clean', 'clear', gulp.parallel('scripts', 'styles', 'imagemin', 'minify', 'serve'));
 }
